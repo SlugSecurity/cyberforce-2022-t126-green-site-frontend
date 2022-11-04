c3d5fb2ac0d78ae92c5085aa244fc5cd590caf7b
daniel <106453412+0xfbad@users.noreply.github.com>
2022-11-04 01:22:22 -0700

    convert to the worst spaghetti code you have ever seen

--- frontend/views.py
+++ frontend/views.py
@@ -1,23 +1,30 @@
 from django.shortcuts import render
-from flask import request
+
+def getStatusText(request):
+	return "Login"
 
 def index(request):
-	return render(request, 'home.html')
+	return render(request, 'home.html', context={ 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request) })
 
 def admin(request):
-	return render(request, 'admin.html')
+	return render(request, 'admin.html', context={ 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request) })
 
 def contact(request):
-	return render(request, 'contact-us.html')
+	if request.method == 'POST':
+		return render(request, 'contact-us.html', context=( {'resp': 'Thank you for contacting us. We will get back to you shortly.', 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request)} ))
+		# do all ur checks here and stuff
+	
+	return render(request, 'contact-us.html', context=({'resp': '', 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request)} ))
 
 def solar(request):
-	return render(request, 'solar-generation.html')
+	return render(request, 'solar-generation.html', context={ 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request) })
 
 def manufacturing(request):
-	return render(request, 'manufacturing.html')
+	return render(request, 'manufacturing.html', context={ 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request) })
 
 def login(request):
 	if request.method == 'POST':
-		return render(request, 'login.html', context={'resp': 'EATED??'})
-	
-	return render(request, 'login.html', context={'resp': 'bruh'})
+		return render(request, 'login.html', context=({'resp': 'Login Failed. Please try again.', 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request)} ))
+		# do all ur checks here and stuff
+
+	return render(request, 'login.html', context=({'resp': '', 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request)} ))

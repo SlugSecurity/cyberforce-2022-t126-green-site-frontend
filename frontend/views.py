7e38eea77d7d9cc3e7cd8bf76be26afcf310a5d8
Brian Mak <eggrollbot@gmail.com>
2022-11-04 21:02:29 -0700

    Format code, improve checks for server error, progress on admin

--- frontend/views.py
+++ frontend/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, redirect
 from django import forms
-import requests, re
+
+import requests
+import re
+
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Name', max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'Name'}))
-    email = forms.EmailField(label='Email', max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'Email'}))
-    phone_number = forms.CharField(label='Phone Number', max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'Phone Number'}))
+    name = forms.CharField(label='Name', max_length=100, required=True,
+                           widget=forms.TextInput(attrs={'placeholder': 'Name'}))
+    email = forms.EmailField(label='Email', max_length=100, required=True,
+                             widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+    phone_number = forms.CharField(label='Phone Number', max_length=100, required=True,
+                                   widget=forms.TextInput(attrs={'placeholder': 'Phone Number'}))
     file = forms.FileField(label='File', required=True)
+
 
 def getStatusText(request):
     if request.COOKIES.get('token') == None:
@@ -17,14 +24,13 @@ def getStatusText(request):
 
 def index(request):
     headers = {'Forwarded': 'for=' + request.META['REMOTE_ADDR']}
+
     try:
         r = requests.get('http://127.0.0.1:8080/api/solar', headers=headers)
     except:
         return render(request, 'home.html', context={
-        'userStateHref': getStatusText(request).lower(),
-        'userStateText': getStatusText(request)})
-
-    solar_arrays = []
+            'userStateHref': getStatusText(request).lower(),
+            'userStateText': getStatusText(request)})
 
     if r.status_code == 200:
         solar_arrays = r.json()
@@ -32,22 +38,52 @@ def index(request):
     return render(request, 'home.html', context={
         'userStateHref': getStatusText(request).lower(),
         'userStateText': getStatusText(request),
-        'solarArrays': solar_arrays})
+        'solarArrays': solar_arrays
+    })
 
 
 def admin(request):
-    return render(request, 'admin.html', context={'userStateHref': getStatusText(request).lower, 'userStateText': getStatusText(request)})
+    headers = {'Forwarded': 'for=' + request.META['REMOTE_ADDR'],
+               'Authorization': 'Bearer ' + request.COOKIES.get('token')}
+
+    try:
+        r = requests.get('http://127.0.0.1:8080/api/files', headers=headers)
+    except:
+        return render(request, 'admin.html', context={'userStateHref': getStatusText(request).lower, 'userStateText': getStatusText(request)})
+
+    if r.status_code == 200:
+        files = r.json()
+
+    if request.method == 'POST':
+        # TODO: Finish processing file download.
+        pass
+
+    try:
+        r = requests.get('http://127.0.0.1:8080/api/emails', headers=headers)
+    except:
+        return render(request, 'admin.html', context={'userStateHref': getStatusText(request).lower, 'userStateText': getStatusText(request)})
+
+    if r.status_code == 200:
+        emails = r.json()
+
+    return render(request, 'admin.html', context={
+        'userStateHref': getStatusText(request).lower,
+        'userStateText': getStatusText(request),
+        'files': files,
+        'emails': emails
+    })
 
 
 def contact(request):
     form = ContactForm()
+
     if request.method == 'POST':
         try:
             contact_name = str(request.POST['name'])
             contact_email = str(request.POST['email'])
             contact_phone = str(request.POST['phone_number'])
             contact_file = request.FILES['file']
-            
+
             if 0 > len(contact_name) > 100:
                 return render(request, 'contact-us.html', context={'form': form, 'resp': 'Name must be between 1 and 100 characters.', 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request)})
             if 0 > len(contact_email) > 100:
@@ -58,24 +94,24 @@ def contact(request):
                 return render(request, 'contact-us.html', context={'form': form, 'resp': 'Phone Number must be between 1 and 20 characters.', 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request)})
             if contact_file.size > 10485760:
                 return render(request, 'contact-us.html', context=({'resp': 'File is larger than 10mb', 'form': form, 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request)}))
-            
+
             try:
                 headers = {'Forwarded': 'for=' + request.META['REMOTE_ADDR']}
                 r_email = requests.post('http://127.0.0.1:8080/api/emails', headers=headers, json={
-                'subject': 'Contact Us Submission - ' + contact_name,
-                'from': contact_name + ' <' + contact_email + '>',
-                'body': 'Name: ' + contact_name + '\nEmail: ' + contact_email + '\nPhone Number: ' + contact_phone})
-                
+                    'subject': 'Contact Us Submission - ' + contact_name,
+                    'from': contact_name + ' <' + contact_email + '>',
+                    'body': 'Name: ' + contact_name + '\nEmail: ' + contact_email + '\nPhone Number: ' + contact_phone})
+
                 r_file = requests.post(
                     'http://127.0.0.1:8080/api/files', headers=headers, files={'file': contact_file})
-                
+
                 if r_email.status_code == 200 and r_file.status_code == 200:
                     return render(request, 'contact-us.html', context=({'hide_submit': True, 'resp': 'Thank you for contacting us. We will get back to you shortly.', 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request)}))
             except:
-               return render(request, 'contact-us.html', context=({'hide_submit': True,'resp': 'Failed to connect to the server. Please try again.', 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request)}))
+                return render(request, 'contact-us.html', context=({'hide_submit': True, 'resp': 'Failed to connect to the server. Please try again.', 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request)}))
         except Exception as e:
             print(e)
-            return render(request, 'contact-us.html', context=({'hide_submit': True,'resp': 'An error occurred. Please try again.', 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request)}))
+            return render(request, 'contact-us.html', context=({'hide_submit': True, 'resp': 'An error occurred. Please try again.', 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request)}))
 
     return render(request, 'contact-us.html', context=({'form': form, 'resp': '', 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request)}))
 

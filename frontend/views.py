adf6f2f5f4a931dabc8e23e3394cd201accfc602
Brian Mak <eggrollbot@gmail.com>
2022-11-04 22:14:35 -0700

    Fix verification of fields, finish up file API calls

--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
 from django import forms
+from django.shortcuts import render, redirect
+from django.http import StreamingHttpResponse
 
 import requests
 import re
@@ -23,6 +24,7 @@ def getStatusText(request):
 
 
 def index(request):
+    solar_arrays = None
     headers = {'Forwarded': 'for=' + request.META['REMOTE_ADDR']}
 
     try:
@@ -43,6 +45,12 @@ def index(request):
 
 
 def admin(request):
+    # No token. Redirect to login page.
+    if 'token' not in request.COOKIES:
+        return redirect('/login')
+
+    files = None
+    emails = None
     headers = {'Forwarded': 'for=' + request.META['REMOTE_ADDR'],
                'Authorization': 'Bearer ' + request.COOKIES.get('token')}
 
@@ -55,8 +63,22 @@ def admin(request):
         files = r.json()
 
     if request.method == 'POST':
-        # TODO: Finish processing file download.
-        pass
+        file_id = request.POST.get('file')
+
+        if file_id is not None:
+            for file in files:
+                if file['id'] == file_id:
+                    try:
+                        r = requests.get(
+                            'http://127.0.0.1:8080/api/files/' + file_id, headers=headers, stream=True)
+
+                        if r.status_code == 200:
+                            return StreamingHttpResponse(streaming_content=r.content, headers={
+                                'Content-Disposition': 'attachment; filename="' + file['name'] + '"'})
+                    except:
+                        pass
+
+                    break
 
     try:
         r = requests.get('http://127.0.0.1:8080/api/emails', headers=headers)
@@ -64,7 +86,8 @@ def admin(request):
         return render(request, 'admin.html', context={'userStateHref': getStatusText(request).lower, 'userStateText': getStatusText(request)})
 
     if r.status_code == 200:
-        emails = r.json()
+        # emails = r.json() # TODO: Substitute this when the API is implemented.
+        emails = []
 
     return render(request, 'admin.html', context={
         'userStateHref': getStatusText(request).lower,
@@ -79,21 +102,28 @@ def contact(request):
 
     if request.method == 'POST':
         try:
-            contact_name = str(request.POST['name'])
-            contact_email = str(request.POST['email'])
-            contact_phone = str(request.POST['phone_number'])
-            contact_file = request.FILES['file']
+            contact_name = request.POST.get('name')
+            contact_email = request.POST.get('email')
+            contact_phone = request.POST.get('phone_number')
+            contact_file = request.FILES.get('file')
+
+            if contact_name is None or contact_email is None or contact_phone is None or contact_file is None:
+                return render(request, 'contact-us.html', context={'form': form, 'resp': 'Missing required field.', 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request)})
 
             if 0 > len(contact_name) > 100:
                 return render(request, 'contact-us.html', context={'form': form, 'resp': 'Name must be between 1 and 100 characters.', 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request)})
+
             if 0 > len(contact_email) > 100:
                 return render(request, 'contact-us.html', context={'form': form, 'resp': 'Email must be between 1 and 100 characters.', 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request)})
+
             if not re.match('([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+', contact_email):
                 return render(request, 'contact-us.html', context={'form': form, 'resp': 'Email is not valid.', 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request)})
+
             if 0 > len(contact_phone) > 20:
                 return render(request, 'contact-us.html', context={'form': form, 'resp': 'Phone Number must be between 1 and 20 characters.', 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request)})
+
             if contact_file.size > 10485760:
-                return render(request, 'contact-us.html', context=({'resp': 'File is larger than 10mb', 'form': form, 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request)}))
+                return render(request, 'contact-us.html', context={'form': form, 'resp': 'File is larger than 10MB', 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request)})
 
             try:
                 headers = {'Forwarded': 'for=' + request.META['REMOTE_ADDR']}
@@ -126,11 +156,20 @@ def manufacturing(request):
 
 def login(request):
     if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if username is None or password is None:
+            return render(request, 'login.html', context=({'resp': 'Missing required field.', 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request)}))
+
+        if 0 > len(username) > 100 or 0 > len(password) > 100:
+            return render(request, 'login.html', context=({'resp': 'Login Failed. Please try again.', 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request)}))
+
         try:
             headers = {'Forwarded': 'for=' + request.META['REMOTE_ADDR']}
             r = requests.post('http://127.0.0.1:8080/api/login', headers=headers, json={
-                'username': request.POST['username'],
-                'password': request.POST['password']})
+                'username': username,
+                'password': password})
         except:
             return render(request, 'login.html', context=({'resp': 'Failed to connect to the server. Please try again.', 'userStateHref': getStatusText(request).lower(), 'userStateText': getStatusText(request)}))
 
